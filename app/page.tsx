2b8139e680e4a966c90a78fe471765a8fc1338e2
Sandro <sdezerio@gmail.com>
2023-10-23 18:01:01 -0500

    finish skill sections.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Divider from "@/components/Divider";
 import Intro from "@/components/Intro";
 import About from "../components/About";
 import Projects from "@/components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Divider />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
