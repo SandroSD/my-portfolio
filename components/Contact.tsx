fbda5ba6aaa8e3b6a41e94310ef09c53bcc0dc2c
Sandro <sdezerio@gmail.com>
2023-10-28 14:39:49 -0500

    contact + send email.

---
+++ components/Contact.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import SectionHeading from "./SectionHeading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitButton from "./SubmitButton";
+import toast from "react-hot-toast";
+
+export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
+  return (
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-20 w-[min(100%, 38rem)] text-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
+      <SectionHeading>Contact Me</SectionHeading>
+      <p className="text-gray-700 -mt-6">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:example@gmail.com ">
+          example@gmail.com
+        </a>{" "}
+        or though this form.
+      </p>
+
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
+        }}
+      >
+        <input
+          type="email"
+          name="senderEmail"
+          className="h-14 rounded-lg borderBlack px-4"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+          name="message"
+          placeholder="Your Message"
+          required
+          maxLength={5000}
+        />
+        <SubmitButton />
+      </form>
+    </motion.section>
+  );
+}
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -7,9 +7,11 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section
@@ -74,13 +76,17 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110  hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all cursor-pointer border border-black/10"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition-all cursor-pointer borderBlack"
           href="/Resume.pdf"
           download={true}
         >
@@ -88,13 +94,13 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1" />
         </a>
         <a
-          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer border border-black/10"
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer borderBlack"
           href="https://linkedin.com"
           target="_blank"
         >
           <BsLinkedin />
         </a>
-        <a className="bg-white text-gray-700 p-4 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer border border-black/10">
+        <a className="bg-white text-gray-700 p-4 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer borderBlack">
           <FaGithubSquare />
         </a>
       </motion.div>
